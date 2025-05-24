38ca8075fcff59164d9b6b54483eb7f56a7c1d56
Ryan Figueredo <ryan.figueredo059@gmail.com>
2025-05-24 06:58:49 -0300

    feat: update seed.js to add vehicle records creation and remove admin user setup

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,29 +1,32 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const passwordHash = await bcrypt.hash("Lu230148$", 10);
-
-  await prisma.user.upsert({
-    where: { email: "gabrielfabri@klfacilities.com.br" },
-    update: {},
-    create: {
-      name: "Admin KL",
-      email: "gabrielfabri@klfacilities.com.br",
-      role: "ADMIN",
-      // caso esteja usando senha diretamente
-      password: passwordHash,
-    },
+  await prisma.vehicle.createMany({
+    data: [
+      {
+        id: "VEICULO_01",
+        placa: "KL-0001",
+        modelo: "Modelo 1",
+        ano: 2020,
+        tipoCombustivel: "FLEX",
+      },
+      {
+        id: "VEICULO_02",
+        placa: "EMO-1F59",
+        modelo: "UNO",
+        ano: 2021,
+        tipoCombustivel: "FLEX",
+      },
+    ],
+    skipDuplicates: true,
   });
-
-  console.log("Usuário admin criado com sucesso.");
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
+  .then(() => {
+    console.log("Veículos cadastrados.");
   })
+  .catch((e) => console.error(e))
   .finally(() => prisma.$disconnect());
